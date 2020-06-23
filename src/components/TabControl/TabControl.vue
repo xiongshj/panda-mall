7f2ddbe5f9ae0a28e2f399bb158d6d582b9af4d4
xiongshj <xiongshjcode@gmail.com>
2020-06-24 00:50:01 +0800

    feat: add home page content
    - add Feature component
    
    - add TabControl component

---
+++ src/components/TabControl/TabControl.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(item,index) in titles"
+      :key="item"
+      :class="{active: index === currentIndex}"
+      class="tab-control-item"
+      @click="itemClick(index)"
+    >
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.tab-control {
+  display: flex;
+  height: 40px;
+  font-size: 15px;
+  line-height: 40px;
+  text-align: center;
+  background: #fff;
+}
+
+.tab-control-item {
+  flex: 1;
+
+  span {
+    padding: 5px;
+  }
+}
+
+.active {
+  color: #ff8396;
+
+  span {
+    border-bottom: 3px solid #ff8396;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,21 +5,126 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend :recommends="recommends"></home-recommend>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+
+    <div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+      <div>~~~---------</div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/navbar/NavBar'
+import TabControl from 'components/TabControl/TabControl'
+
 import HomeSwiper from './components/HomeSwiper'
 import HomeRecommend from './components/HomeRecommend'
+import FeatureView from './components/FeatureView'
+
 import { getHomeMultidata } from '@/api/home'
 
 export default {
   name: 'Home',
   components: {
     NavBar,
+    TabControl,
     HomeSwiper,
-    HomeRecommend
+    HomeRecommend,
+    FeatureView
   },
   data() {
     return {
@@ -37,8 +142,22 @@ export default {
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
+
 .home-nav {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 9;
   color: #fff;
   background: #ff8e96;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
