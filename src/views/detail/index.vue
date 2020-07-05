bcada08dba826f90c32f61d4357b66b1baaca66d
xiongshj <xiongshjcode@gmail.com>
2020-07-05 23:18:39 +0800

    feat: 完成详情页商品基本信息的展示

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -2,25 +2,29 @@
   <div id="detail">
     <detail-nav-bar />
     <detail-swiper :top-images="topImages" />
+    <detail-base-info :goods="goods" />
   </div>
 </template>
 
 <script>
 import DetailNavBar from './components/DetailNavBar'
 import DetailSwiper from './components/DetailSwiper'
+import DetailBaseInfo from './components/DetailBaseInfo'
 
-import { getDetail } from '@/api/detail'
+import { getDetail, Goods } from '@/api/detail'
 
 export default {
   name: 'Detail',
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo
   },
   data() {
     return {
       id: null,
-      topImages: []
+      topImages: [],
+      goods: {}
     }
   },
   created() {
@@ -31,7 +35,16 @@ export default {
     getDetailData() {
       getDetail(this.id).then(res => {
         console.log(res)
-        this.topImages = res.result.itemInfo.topImages
+        const data = res.result
+        // 获取顶部的图片轮播数据
+        this.topImages = data.itemInfo.topImages
+
+        // 获取商品信息
+        this.goods = new Goods(
+          data.itemInfo,
+          data.columns,
+          data.shopInfo.services
+        )
       })
     }
   }
